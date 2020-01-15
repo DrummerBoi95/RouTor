c764e598d1deb6b7dd4380bbbc77b73d3a60af52
Lawrence Paine <l.paine@hotmail.co.uk>
2020-01-15 21:28:48 +0000

    Back  End & Front End Upload

---
+++ RouTor.Backend/SilentWebBrowser.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RouTor.Backend
+{
+    public class SilentWebBrowser
+    {
+        public WebBrowser WebBrowser { get; set; }
+
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string LANIP { get; set; }
+
+        public SilentWebBrowser()
+        {
+
+        }
+                
+        public void NavigateLogin(string strURL)
+        {
+            WebBrowser.Navigate(strURL);
+            System.Threading.Thread.Sleep(2000);
+        }  
+        
+        public void PerformLogin()
+        {
+            HtmlElementCollection theElementCollection;
+            theElementCollection = WebBrowser.Document.GetElementsByTagName("input");
+            foreach (HtmlElement curElement in theElementCollection)
+            {
+                string controlName = curElement.GetAttribute("name").ToString();
+                if (controlName == "UserNameTextBox")
+                    curElement.SetAttribute("Value", UserName);
+                else if (controlName == "PasswordTextBox")
+                    curElement.SetAttribute("Value", Password);
+            }
+
+            theElementCollection = WebBrowser.Document.GetElementsByTagName("input");
+            foreach (HtmlElement curElement in theElementCollection)
+            {
+                if (curElement.GetAttribute("value").Equals("Login"))
+                    curElement.InvokeMember("click");
+            }
+
+            WebBrowser.Document.GetElementById("AuthName").SetAttribute("value", UserName);
+            WebBrowser.Document.GetElementById("Display").SetAttribute("value", Password);
+            WebBrowser.Document.GetElementById("submit").InvokeMember("click");
+            // WebBrowser1.Document.GetElementById("network").InvokeMember("click")
+            System.Threading.Thread.Sleep(50000);
+
+            LoadPages();
+            if (WebBrowser.Url.ToString().Contains("index"))
+            {
+                LoadPages();
+                WebBrowser.DocumentCompleted += DocumentCompleted;
+            }
+        }
+
+        private void DocumentCompleted(System.Object sender, System.Windows.Forms.WebBrowserDocumentCompletedEventArgs e)
+        {
+         
+        }
+        
+        private void LoadPages()
+        {
+            WebBrowser.Document.GetElementById("network-homeNetworking").InvokeMember("click");
+            WebBrowser.Document.GetElementById("network-homeNetworking").InvokeMember("click");
+
+            System.Threading.Thread.Sleep(10000);
+            WebBrowser.Document.GetElementById("t6").InvokeMember("click");
+            WebBrowser.Document.GetElementById("wakeOnLanIPAddr").SetAttribute("value", LANIP);
+            WebBrowser.Document.GetElementById("str1").SetAttribute("value", "00");
+            WebBrowser.Document.GetElementById("str2").SetAttribute("value", "e0");
+            WebBrowser.Document.GetElementById("str3").SetAttribute("value", "4c");
+            WebBrowser.Document.GetElementById("str4").SetAttribute("value", "29");
+            WebBrowser.Document.GetElementById("str5").SetAttribute("value", "13");
+            WebBrowser.Document.GetElementById("str6").SetAttribute("value", "8c");
+            WebBrowser.Document.GetElementById("wakeUpBtn").InvokeMember("click");
+        }
+    }    
+}
---
+++ RouTor.Client/MainActivity.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Support.Wearable.Views;
+using Android.Support.V4.App;
+using Android.Support.V4.View;
+using Java.Interop;
+using Android.Views.Animations;
+using RouTor.Backend;
+
+namespace RouTor.Client
+{
+    [Activity(Label = "RouTor.Client", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.RoundMain);
+
+            var v = FindViewById<WatchViewStub>(Resource.Id.watch_view_stub);
+            v.LayoutInflated += delegate
+            {
+                
+                Button button = FindViewById<Button>(Resource.Id.myButton);
+
+                button.Click += delegate
+                {
+                    //Perform router login
+                    SilentWebBrowser oBrowser = new SilentWebBrowser();
+
+                    oBrowser.UserName = Resources.GetString(Resource.String.Username);
+                    oBrowser.Password = Resources.GetString(Resource.String.Password);
+                    oBrowser.LANIP = Resources.GetString(Resource.String.LANIP);
+
+                    oBrowser.NavigateLogin("http://159.253.72.79/login/login.html");
+
+                    oBrowser.PerformLogin();
+                };
+            };
+        }
+    }
+}
+
+
